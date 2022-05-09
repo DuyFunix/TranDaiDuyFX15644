6997471f32a1fb727d0cfa57ac6b820c04917ec8
Tran Dai Duy <duytdfx15644@funix.edu.vn>
2022-05-09 23:09:57 +0700

    Component Part 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-
+import React, { Component } from "react";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import MeNu from "./components/MenuComponent";
+import { DISHES } from "./components/share/dishes";
 class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      dishes: DISHES,
+    };
+  }
   render() {
     return (
       <div className="App">
         <Navbar dark color="primary">
-          <div className='primary'>
-            <NavbarBrand href = '/'>Ristorante Con Fusion</NavbarBrand>
+          <div className="primary">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-          
         </Navbar>
-        
-        <Menu></Menu>
+
+        <MeNu dishes={this.state.dishes}></MeNu>
       </div>
     );
   }
